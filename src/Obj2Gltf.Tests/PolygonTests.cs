fc4025921649eec42950a23a662de51b5ec50f7d
Jifeng <zhangjifeng_nihao@163.com>
2019-01-07 18:09:17 +0800

    more point in polygon tests

--- src/Obj2Gltf.Tests/PolygonTests.cs
+++ src/Obj2Gltf.Tests/PolygonTests.cs
@@ -16,12 +16,12 @@ namespace Arctron.Obj2Gltf.Tests
                 new Vec2(0.0, 0.0), new Vec2(1.0, 0.0), new Vec2(1.0,1.0), new Vec2(0.5,0.5), new Vec2(0.0,1.0)
             };
             var tol = 1e-10;
-            //Assert.Equal(PolygonPointRes.Vetex, PolygonUtil.CrossTest(new Vec2(0.0, 0.0), pnts, tol));
-            //Assert.Equal(PolygonPointRes.Edge, PolygonUtil.CrossTest(new Vec2(0.1, 0.0), pnts, tol));
-            //Assert.Equal(PolygonPointRes.Outside, PolygonUtil.CrossTest(new Vec2(0.6, 0.5), pnts, tol));
-            //Assert.Equal(PolygonPointRes.Outside, PolygonUtil.CrossTest(new Vec2(0.5, 0.500001), pnts, tol));
+            Assert.Equal(PolygonPointRes.Vetex, PolygonUtil.CrossTest(new Vec2(0.0, 0.0), pnts, tol));
+            Assert.Equal(PolygonPointRes.Edge, PolygonUtil.CrossTest(new Vec2(0.1, 0.0), pnts, tol));
+            Assert.Equal(PolygonPointRes.Outside, PolygonUtil.CrossTest(new Vec2(0.5, 0.6), pnts, tol));
+            Assert.Equal(PolygonPointRes.Outside, PolygonUtil.CrossTest(new Vec2(0.5, 0.500001), pnts, tol));
             Assert.Equal(PolygonPointRes.Inside, PolygonUtil.CrossTest(new Vec2(0.5, 0.499999), pnts, tol));
-            //Assert.Equal(PolygonPointRes.Outside, PolygonUtil.CrossTest(new Vec2(1.5, 0.5), pnts, tol));
+            Assert.Equal(PolygonPointRes.Outside, PolygonUtil.CrossTest(new Vec2(1.5, 0.5), pnts, tol));
         }
     }
 }
